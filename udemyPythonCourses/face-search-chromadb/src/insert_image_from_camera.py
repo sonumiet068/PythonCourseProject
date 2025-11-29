14224a218d65c0d4dabab8b64d858b858dd38843
sonumiet068 <sonumiet068@gmail.com>
2025-11-29 17:33:49 +0530

    Modify insert image in which left right photo tooks

--- udemyPythonCourses/face-search-chromadb/src/insert_image_from_camera.py
+++ udemyPythonCourses/face-search-chromadb/src/insert_image_from_camera.py
@@ -4,26 +4,36 @@ from db_setup import get_chroma_collection
 from embedder import get_embedding_from_image
 
 collection = get_chroma_collection()
-print("Now,Capture a face to INSERT into database")
-img = capture_from_camera("Insert mode")
-
-if img is None:
-  print("No image captured !")
-  exit()
-
-embedding = get_embedding_from_image(img)
-if embedding is None:
-  print("No face Detected!")
-  exit()
 name = input("Enter person name:")
+count = 3
+ids = []
+embeddings=[]
+while count>=0:
+  if(count==3):
+    print("Capture left face")
+  elif count==2:
+    print("Capture right face")
+  elif count==1:
+    print("Now,Capture slightly up face ")
+  elif count ==0:
+    print("Now,Capture slightly down face")
 
-face_id = f"{name}_{uuid.uuid4()}"
-collection.add(
-  ids=[face_id],
-  embeddings=[embedding],
-  metadatas=[{"name":name}],
-  documents=["Capture face"],
-)  
+  img = capture_from_camera("Insert mode")
+  if img is None:
+    print("No image captured,Please try again!")
+    continue
+  emb = get_embedding_from_image(img)
+  if emb is None:
+    print("No face Detected,Please try again!")
+    continue
+  collection.add(
+    ids=[f"{name}_{uuid.uuid4()}"],
+    embeddings=[emb],
+    metadatas=[{"name":name}],
+    documents=["Capture face"],
+  ) 
+  count=count-1
+    
 print("Face inserted successfully!")
   
 
